ee3527d55554a635773e211a73c7ad95c1ad1b36
Alexis DRAI <drai.alexis@gmail.com>
2021-11-29 13:08:25 +0100

    Initial commit, set up basic structure

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# 2048-c
+# 2048 in C
+2-person project to code 2048 in C, using SDL
---
+++ toolbox.h
@@ -0,0 +1,6 @@
+#ifndef INC_2048_C_TOOLBOX_H
+#define INC_2048_C_TOOLBOX_H
+
+#include <stdio.h>
+
+#endif //INC_2048_C_TOOLBOX_H
