d5ecaf8b05a9f4b6d1424fa940f566ff306c8766
draialexis <drai.alexis@gmail.com>
2021-12-11 23:43:03 +0100

    Implement score in SDL, start on timer

--- include/toolbox.c
+++ include/toolbox.c
@@ -1,10 +1,5 @@
 #include "toolbox.h"
 
-void Purge() {
-    int c;
-    while ((c = getchar()) != '\n' && c != EOF) {}
-}
-//TODO improve purge
 int isFOpen(FILE *f, char *f_name) {
     if (f == NULL) {
         printf("error while opening file '%s'\n", f_name);
--- include/toolbox.h
+++ include/toolbox.h
@@ -5,15 +5,9 @@
 #include <stdlib.h>
 #include <time.h>
 
-//don't use DEBUG in replit...
 #define DEBUG printf("file %s; line %d\n", __FILE__, __LINE__);
 #define FAIL_OUT printf("failing out\n");DEBUG exit(EXIT_FAILURE);
 #define MALLOC_FAIL printf("!_malloc failed_!\n"); FAIL_OUT
-
-/**
- * empties stdin from all superfluous characters, including final \\n
- */
-void Purge();
 
 /**
  * checks if a file was open successfully; exits with error if not
